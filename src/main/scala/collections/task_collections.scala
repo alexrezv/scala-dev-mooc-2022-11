09851b1c7dad8241221204f2aa978809aeb8cc40
Alexander Rezvantsev <ramspb95@gmail.com>
2023-01-14 10:39:18 +0300

    PT2: numbers to numeric string

--- src/main/scala/collections/task_collections.scala
+++ src/main/scala/collections/task_collections.scala
@@ -34,7 +34,15 @@ object task_collections {
    * HINT: Для всех возможных комбинаций чисел стоит использовать Map
    */
   def numbersToNumericString(text: String): String = {
-    ""
+    def mapping = Map(
+      0 -> "zero", 1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four",
+      5 -> "five", 6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine"
+    )
+
+    text.toList.collect {
+      case it if it.isDigit => mapping(it.toString.toInt)
+      case it => it.toString
+    }.mkString
   }
 
   /**
