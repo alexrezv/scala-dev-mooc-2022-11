5c0abe66d3cde60555c452304f635325498dbf4d
Alexander Rezvantsev <ramspb95@gmail.com>
2023-02-11 13:40:35 +0300

    hw7: extensions, magnet pattern, opaque type

--- scala3proj/src/main/scala/scala_3/extendingclasses/homework.scala
+++ scala3proj/src/main/scala/scala_3/extendingclasses/homework.scala
@@ -5,56 +5,73 @@ import scala_3.extendingclasses.Extensionmethods.Circle
 
 object homework1 {
   extension (x: String)
-    def + ?????
+    private def :+:(y: String): Int = x.toInt + y.toInt
 
-    @main def part1Ex(): Unit ={
-      println("1" + "33")
-    }
+  @main def part1Ex(): Unit = {
+    println("1" :+: "33")  // 34
+  }
 }
 
 object homework2 {
-  enum CompletionArg {
-    ???
-    //см приведенную ссылку
-  }
 
-  object CompletionArg {
-    given fromString: Conversion[String, CompletionArg] = ???
+  enum CompletionArg:
+    case ShowItIsString(s: String)
+    case ShowItIsInt(i: Int)
+    case ShowItIsFloat(f: Float)
 
-    given fromInt: Conversion[Int, CompletionArg] = ???
+  private object CompletionArg:
+    given fromString: Conversion[String, CompletionArg] = ShowItIsString(_)
 
-    given fromFloat: Conversion[Float, CompletionArg] = ???
-  }
+    given fromInt: Conversion[Int, CompletionArg] = ShowItIsInt(_)
+
+    given fromFloat: Conversion[Float, CompletionArg] = ShowItIsFloat(_)
+  end CompletionArg
+
+
   import CompletionArg.*
 
-  @main def part2Ex(): Unit ={
-    println(Completions.complete("String"))
-    println(Completions.complete(1))
-    println(Completions.complete(7f))
+  private def complete(arg: CompletionArg): String = arg match
+    case ShowItIsString(s) => s"It's a string: $s"
+    case ShowItIsInt(i) => s"It's an Int: $i"
+    case ShowItIsFloat(f) => s"It's a Float: $f"
+
+  @main def part2Ex(): Unit = {
+    println(complete("String"))
+    println(complete(1))
+    println(complete(7.7f))
   }
+
 }
 
 
 object homework3 {
   opaque type Logarithm = Double
 
-  object Logarithm{
-    //см приведенную ссылку
-  }
+  object Logarithm:
+    def apply(d: Double): Logarithm = math.log(d)
+
+    def safe(d: Double): Option[Logarithm] =
+      if d > 0.0 then Some(math.log(d)) else None
+
+  end Logarithm
+
 
   extension (x: Logarithm)
-    def toDouble: ???
-    def + (y: Logarithm): ???
-    def * (y: Logarithm): ???
+    def toDouble: Double = math.exp(x)
+    def +(y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
+    def *(y: Logarithm): Logarithm = x + y
 
+}
 
-  @main def part3Ex(): Unit ={
-    import homework3.Logarithm
+@main def part3Ex(): Unit = {
+  import homework3.Logarithm
 
-    val l = Logarithm(1.0)
-    val l2 = Logarithm(2.0)
-    val l3 = l * l2
-    val l4 = l + l2
+  val l: Logarithm = Logarithm(1.0)
+  val l2: Logarithm = Logarithm(2.0)
+  val l3: Logarithm = l * l2
+  val l4: Logarithm = l + l2
 
-  }
+  println(l3.toDouble)
+  println(l4.toDouble)
+
 }
